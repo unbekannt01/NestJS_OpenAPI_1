7a2b752be0c1993837616d0b623b84af8524d80c
Prashant <pkargathara7401@gmail.com>
2025-03-24 17:01:15 +0530

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const config = new DocumentBuilder()
-  //   .setTitle('API')
-  //   .setDescription('This is Open API')
-  //   .setVersion('1.0')
-  //   .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
-  //   .build();
-  // const documentFactory = () => SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, documentFactory);
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('This is Open API')
+    .setVersion('1.0')
+    .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
