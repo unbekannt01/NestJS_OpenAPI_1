d2886d0405b9d1b506d31a4b9d1837cc6a9c654b
Prashant <pkargathara7401@gmail.com>
2025-03-26 18:53:27 +0530

    No Any Update Just Commit

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);                                                          
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
