356907a5a0502f046f9894b872c1ec1bfd69cc50
Prashant <pkargathara7401@gmail.com>
2025-04-29 18:17:22 +0530

    small update

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-jwt';
 import { Request } from 'express';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -16,8 +17,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: JwtPayload) {
     // Return user info based on the JWT payload
-    return { userId: payload.sub, username: payload.username };
+    return { userId: payload.id, email: payload.email, role: payload.role };
   }
 }
