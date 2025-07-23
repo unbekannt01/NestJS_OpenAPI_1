6ee54f3b8dbc3a861b985ddc0e97e8f5f895e781
Prashant <pkargathara7401@gmail.com>
2025-07-23 18:37:29 +0530

    Update in main.ts

--- src/main.ts
+++ src/main.ts
@@ -57,7 +57,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*' ,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
